5fbbaa9cf27cadde7137634f20f2c82aa7beeb1a
xopazaru0343 <avinope2@gmail.com>
2025-12-03 17:07:12 -0300

    copium3

--- src/day1part2.java
+++ src/day1part2.java
@@ -16,18 +16,29 @@ public class day1part2 {
                     password++;
                     subtract-=100;
                 }
-                if(Math.floorMod(x-subtract, 100) > x){
+                if(x == 0){
+                    System.out.println("a");
+                }
+                if(Math.floorMod(x-subtract, 100) > x || Math.floorMod(x-subtract, 100) == 0){
                     password++;
                 }
                 x = Math.floorMod(x-subtract, 100);
             } else if(command.substring(0,1).equals("R")){
                 int add = Integer.parseInt(command.substring(1));
-                x += add;
-                password += x / 100;
-                x = Math.floorMod(x, 100);
+                while(add>=100){
+                    x = Math.floorMod(x+100, 100);
+                    password++;
+                    add-=100;
+                }
+                if(x == 0){
+                    System.out.println("a");
+                }
+                if(Math.floorMod(x+add, 100) < x || Math.floorMod(x+add, 100) == 0){
+                    password++;
+                }
         }
-        System.out.println(password);
     }
+        System.out.println(password);
 }
 }
 
