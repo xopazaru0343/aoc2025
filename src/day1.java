acd889152faa90c8b53225a36003a7f5dc63883a
xopazaru0343 <avinope2@gmail.com>
2025-12-03 15:22:30 -0300

    now done i hope

--- src/day1.java
+++ src/day1.java
@@ -6,16 +6,21 @@ public class day1 {
         BufferedReader breeder = new BufferedReader(reader); // keepo
         Object[] input = breeder.lines().toArray();
         int x = 50;
+        int password = 0;
         for(Object command1: input){
             String command = command1.toString();
             if(command.substring(0,1).equals("L")){
-                int subtract = Integer.parseInt(command.substring(1));
+                int subtract = Math.floorMod(Integer.parseInt(command.substring(1)), 100);
                 x = Math.floorMod(x - subtract, 100);
             } else if(command.substring(0,1).equals("R")){
-                int add = Integer.parseInt(command.substring(1));
+                int add = Math.floorMod(Integer.parseInt(command.substring(1)), 100);
                 x = Math.floorMod(x + add, 100);
             }
+            if(x == 0){
+                password++;
+            }
         }
-        System.out.println(x);
+        System.out.println(password);
     }
 }
+
