039b3c768231165c01c65e5c26c8658001e25e06
xopazaru0343 <avinope2@gmail.com>
2025-12-03 20:01:38 -0300

    day 2 part 2 done

--- src/day2part2.java
+++ src/day2part2.java
@@ -1,29 +1,39 @@
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-public class day2 {
+import java.util.Collections;
+import java.util.HashSet;
+
+public class day2part2 {
     public static void main(String[] args) throws Exception {
         FileReader reader = new FileReader("./inputday2");
         BufferedReader breeder = new BufferedReader(reader); // keepo
         String[] input = breeder.readLine().split(",");
 
-        ArrayList<Long> set = new ArrayList<Long>();
+        HashSet<Long> set = new HashSet<Long>();
         long finaldestination = 0;
 
         for(long i = 0; i<100000; i++){
             String a = Long.toString(i);
-            set.add(Long.parseLong(a.concat(a)));
+            String atemp = a;
+            while(atemp.concat(a).length() <= 10){
+            atemp = atemp.concat(a);
+            set.add(Long.parseLong(atemp));
+            }
         }
+
+        ArrayList<Long> list = new ArrayList<Long>(set);
+        Collections.sort(list);
 
         for(String range: input){
             String[] bounds = range.split("-");
             long bound1 = Long.parseLong(bounds[0]);
             long bound2 = Long.parseLong(bounds[1]);
-            int a = BinarySearchLeft(set, bound1);
+            int a = BinarySearchLeft(list, bound1);
             System.out.println(range);
-            while(a < set.size() && set.get(a) >= bound1 && set.get(a) <= bound2){
-                finaldestination += set.get(a);
+            while(a < list.size() && list.get(a) >= bound1 && list.get(a) <= bound2){
+                System.out.println(list.get(a));
+                finaldestination += list.get(a);
                 a++;
             }
         }
