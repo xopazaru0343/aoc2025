646b16afcc1de8428fd30daf52c3653ce5da628f
xopazaru0343 <avinope2@gmail.com>
2025-12-03 16:43:54 -0300

    copium2

--- src/day1part2.java
+++ src/day1part2.java
@@ -10,24 +10,24 @@ public class day1part2 {
         for(Object command1: input){
             String command = command1.toString();
             if(command.substring(0,1).equals("L")){
-                int subtract = Integer.parseInt(command.substring(1));
-                if(Integer.max(x, subtract) == subtract){
-                    subtract -= x;
-                    x = 0;
+                int subtract = Integer.parseInt(command.substring(1)); 
+                while(subtract>=100){
+                    x = Math.floorMod(x-100, 100);
+                    password++;
+                    subtract-=100;
+                }
+                if(Math.floorMod(x-subtract, 100) > x){
                     password++;
                 }
-                x -= subtract;
-                password += Math.abs(x / 100);
-                x = Math.floorMod(x, 100);
-
+                x = Math.floorMod(x-subtract, 100);
             } else if(command.substring(0,1).equals("R")){
                 int add = Integer.parseInt(command.substring(1));
                 x += add;
-                password += Math.abs(x / 100);
+                password += x / 100;
                 x = Math.floorMod(x, 100);
-            }
         }
         System.out.println(password);
     }
+}
 }
 
